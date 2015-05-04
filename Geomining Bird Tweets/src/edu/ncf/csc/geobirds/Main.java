4e5c05c63c75ec9adf45002b1e17c06c9f9cca7d
Sky Kistler <skyler.kistler14@ncf.edu>
2015-05-03 20:40:17 -0400

    progress

--- Geomining Bird Tweets/src/edu/ncf/csc/geobirds/Main.java
+++ Geomining Bird Tweets/src/edu/ncf/csc/geobirds/Main.java
@@ -1,6 +1,11 @@
 package edu.ncf.csc.geobirds;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.List;
 
 import twitter4j.GeoLocation;
 import twitter4j.Query;
@@ -11,28 +16,57 @@ import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
 
 public class Main {
-	public static void main(String[] args) throws TwitterException {
-		Twitter twitter = TwitterFactory.getSingleton();
+	static Twitter twitter = TwitterFactory.getSingleton();
 
-		int numberOfTweets = 2048;
-		Query query = new Query("eagle");
+	public static void main(String[] args) throws TwitterException, IOException {
+		BufferedReader in = new BufferedReader(new FileReader("commonnames.txt"));
+
+		System.out.println("===============================\n" + "Starting Egyptian Goose Geomine\n      Built by the NCCSC\n" + "===============================");
+		ArrayList<BirdTweet> tweets = new ArrayList<BirdTweet>();
+		for (String line; (line = in.readLine()) != null;) {
+			tweets.addAll(doSearch(line));
+		}
+		in.close();
+		System.out.println();
+		tweets = removeDuplicates(tweets);
+
+		int records = 0;
+		PrintWriter print = new PrintWriter("egyptiangoosetweets.csv");
+		for (BirdTweet bt : tweets) {
+			if (bt.tweet.getGeoLocation() != null) {
+				print.println(records + "," + bt);
+				records++;
+				System.out.println(bt);
+			}
+		}
+		System.out.println("\nFound a grand total of " + tweets.size() + " tweets");
+
+		print.flush();
+		print.close();
+	}
+
+	public static ArrayList<BirdTweet> doSearch(String q) {
+		System.out.println("\nSearching for: " + q);
+		int maxNumberOfTweets = 2048;
+		Query query = new Query(q);
 		query.setSince("2012-01-01");
 
 		long lastID = Long.MAX_VALUE;
-		ArrayList<Status> tweets = new ArrayList<Status>();
-		while (tweets.size() < numberOfTweets) {
-			if (numberOfTweets - tweets.size() > 100)
+		ArrayList<BirdTweet> tweets = new ArrayList<BirdTweet>();
+		while (tweets.size() < maxNumberOfTweets) {
+			if (maxNumberOfTweets - tweets.size() > 100)
 				query.setCount(100);
 			else
-				query.setCount(numberOfTweets - tweets.size());
+				query.setCount(maxNumberOfTweets - tweets.size());
 			try {
 				QueryResult result = twitter.search(query);
-				tweets.addAll(result.getTweets());
-				System.out.println("Gathered " + tweets.size() + " tweets");
-				for (Status t : tweets)
-					if (t.getId() < lastID)
-						lastID = t.getId();
-
+				if (result.getTweets().size() == 0)
+					break;
+				tweets.addAll(convertToBirdTweets(result.getTweets(), query));
+				System.out.println("Gathered " + result.getTweets().size() + " tweets...");
+				for (BirdTweet t : tweets)
+					if (t.tweet.getId() < lastID)
+						lastID = t.tweet.getId();
 			}
 
 			catch (TwitterException te) {
@@ -41,14 +75,45 @@ public class Main {
 			query.setMaxId(lastID - 1);
 		}
 
-		int sample = 0;
-		for (Status s : tweets) {
-			GeoLocation geodata = s.getGeoLocation();
-			if (geodata == null || geodata.getLatitude() == 0 || geodata.getLongitude() == 0)
-				continue;
-			String lat = String.valueOf(geodata.getLatitude());
-			String lon = String.valueOf(geodata.getLongitude());
-			System.out.println(sample++ + " " + query.getQuery() + " " + lat + ", " + lon);
+		System.out.println("Found a total of " + tweets.size() + " tweets");
+
+		return tweets;
+	}
+
+	public static ArrayList<BirdTweet> convertToBirdTweets(List<Status> sts, Query q) {
+		ArrayList<BirdTweet> bts = new ArrayList<BirdTweet>();
+		for (Status s : sts) {
+			bts.add(new BirdTweet(s, q));
+		}
+		return bts;
+	}
+
+	public static ArrayList<BirdTweet> removeDuplicates(ArrayList<BirdTweet> list) {
+		ArrayList<Long> seen = new ArrayList<Long>();
+		ArrayList<BirdTweet> no_dupes = new ArrayList<BirdTweet>();
+		for (BirdTweet bt : list) {
+			if (!seen.contains((Long) bt.tweet.getId())) {
+				seen.add((Long) bt.tweet.getId());
+				no_dupes.add(bt);
+			}
+		}
+		return no_dupes;
+	}
+
+	static class BirdTweet {
+		Status tweet;
+		String query;
+
+		public BirdTweet(Status s, Query q) {
+			tweet = s;
+			query = q.getQuery();
+		}
+
+		public String toString() {
+			GeoLocation geodata = tweet.getGeoLocation();
+			String lat = geodata == null ? "NA" : String.valueOf(geodata.getLatitude());
+			String lon = geodata == null ? "NA" : String.valueOf(geodata.getLongitude());
+			return tweet.getId() + "," + query + "," + lat + "," + lon + "," + tweet.getCreatedAt();
 		}
 	}
 }
